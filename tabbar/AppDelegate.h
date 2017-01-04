3d271e19357f87dd156e9c20bc3fd6f089a33e05
Z-Victory <310346124@qq.com>
2017-01-04 11:02:48 +0800

    Initial Commit

---
+++ tabbar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tabbar
+//
+//  Created by IOS on 17/1/4.
+//  Copyright © 2017年 IOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tabbar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tabbar
+//
+//  Created by IOS on 17/1/4.
+//  Copyright © 2017年 IOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
